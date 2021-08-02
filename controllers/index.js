db47f427ee59acd067383b45ff268310b7d37569
Arnold Lee <arnoldlee60@gmail.com>
2021-08-02 01:21:45 -0500

    changing stuff

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,4 @@ const homeRoutes = require('./home-routes.js');
 //const api = require('./api');
 router.use('/', homeRoutes);
 //router.use('/', api);
-module.exports = router;
+module.exports = router; 
