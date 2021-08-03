3e7f70fee273874bb48c1b41f2556ad1e40335fe
Arnold Lee <arnoldlee60@gmail.com>
2021-08-03 12:30:55 -0500

    fixed login problem with speed? doesnt immediately change for some reason

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       //document.location.replace('/');
-      alert("ok")
+      //alert("ok")
     } else {
       alert('Failed to log in.');
     }
