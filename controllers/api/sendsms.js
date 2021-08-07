d6a56653050a67bb0f29e6ee6fe45409db9f9313
Isack Ramirez <isack.ramirez1@gmail.com>
2021-08-07 10:10:54 -0500

    pushing for version

--- controllers/api/sendsms.js
+++ controllers/api/sendsms.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+
 const client = require("twilio")(accountSid,authToken);
 
-function sendSMS(recipientNumber,messageBody){
 client.messages.create({
-    to: recipientNumber,
-    from : '+1 210 750 3157',
-    body: messageBody
 
+    to: '+1 210 464 0710',
+    from : '+1 210 750 3157',
+    body: "sent from my desktop"
 })
 .then((message) => console.log(message.sid));
-
-}
-
-
