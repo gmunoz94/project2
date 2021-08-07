b70d458af7a07866d8856450851ff3cc81601d1e
marco <marcodotr@gmial.com>
2021-08-06 19:25:19 -0500

    savebtn put attempt

--- public/js/patient.js
+++ public/js/patient.js
@@ -25,3 +25,31 @@ const patientFormHandler = async (event) => {
 }
 
 document.querySelector('.add-patient').addEventListener('submit', patientFormHandler);
+
+const putFormHandler = async (event) => {
+    event.preventDefault();
+
+    const first_name = document.querySelector('#ptFirstName').value.trim();
+    const last_name = document.querySelector('#ptLastName').value.trim();
+    const email = document.querySelector('#ptEmail').value.trim();
+    const phone_number = document.querySelector('#ptPhoneNumber').value.trim();
+    const dateOfBirth = document.querySelector('#ptDOB').value.trim();
+
+    console.log(first_name)
+
+    if (first_name && last_name && email && phone_number && dateOfBirth) {
+        const response = await fetch('/api/patient/:id', {
+            method: 'PUT',
+            body: JSON.stringify({ first_name, last_name, email, phone_number, dateOfBirth }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // document.location.replace('/');
+        } else {
+            alert('Failed to add Patient')
+        }
+    }
+}
+
+document.querySelector('#savebtn').addEventListener('submit', patientFormHandler);
